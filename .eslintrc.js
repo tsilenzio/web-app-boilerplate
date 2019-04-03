052172eefe8f076644ba45bf4d6658a4b2cab68c
Taylor Silenzio <taylor@silenz.io>
2019-04-03 12:40:20 -0400

    added eslint with typescript support

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports =  {
+  // Specifies the ESLint parser
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions:  {
+    // Allows for the parsing of modern ECMAScript features
+    ecmaVersion:  2018,
+    // Allows for the use of imports
+    sourceType:  'module',
+  },
+  rules:  {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
